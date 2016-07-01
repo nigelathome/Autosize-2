2b6a8232f467dec80692024baa7fb3abd721cbb8
Nigel Lee <lihui8@le.com>
2016-07-01 11:02:30 +0800

    add 4 buttons and a view into this project ，and those property which are used to describe them

--- Autosize2/Autosize2/ViewController.h
+++ Autosize2/Autosize2/ViewController.h
@@ -9,7 +9,11 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
+@property (weak, nonatomic) IBOutlet UIButton *actionButton1;
+@property (weak, nonatomic) IBOutlet UIButton *actionButton2;
+@property (weak, nonatomic) IBOutlet UIButton *actionButton3;
+@property (weak, nonatomic) IBOutlet UIButton *actionButton4;
+@property (weak, nonatomic) IBOutlet UIView *contentView;
 
 @end
 
